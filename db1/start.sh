ce75c3669e65cadc18b9f7c12c968b6eba949edb
kicapman1x <ahhannan995@gmail.com>
2025-11-23 14:30:59 +0800

    yeet

--- db1/start.sh
+++ db1/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$MSQL_DB1_HOME/bin/mysql --defaults-file=$MSQL_DB1_HOME/config/my.cnf &
+$MSQL_DB1_HOME/bin/mysqld --defaults-file=$MSQL_DB1_HOME/config/my.cnf &
--- db2/start.sh
+++ db2/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$MSQL_DB2_HOME/bin/mysql --defaults-file=$MSQL_DB2_HOME/config/my.cnf &
+$MSQL_DB2_HOME/bin/mysqld --defaults-file=$MSQL_DB2_HOME/config/my.cnf &
